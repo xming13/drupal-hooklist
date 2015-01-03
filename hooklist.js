9c14ecb8a31aa4d9291723bfcebc90b2289726e3
xming13 <xiaoming33@gmail.com>
2015-01-03 14:32:28 +0800

    change js code to self-executing annoymous function. also easier to use $ for jquery.

--- hooklist.js
+++ hooklist.js
@@ -1,17 +1,21 @@
-jQuery(document).ready(function() {
-   jQuery('.toggle').click(function() {
+(function() {
+  var $ = jQuery;
+    
+  $(document).ready(function() {
+    $('.toggle').click(function() {
       var isShow = this.innerHTML === '+';
       var sample = this.parentNode.nextElementSibling;
       
       if (sample) {
-          if (isShow) {
-              jQuery(sample).fadeIn('fast');
-              this.innerHTML = '-';
-          }
-          else {
-              jQuery(sample).fadeOut('fast');
-              this.innerHTML = '+';
-          }
+        if (isShow) {
+          jQuery(sample).fadeIn('fast');
+          this.innerHTML = '-';
+        }
+        else {
+          jQuery(sample).fadeOut('fast');
+          this.innerHTML = '+';
+        }
       }
    }); 
-});
+  });
+})();
